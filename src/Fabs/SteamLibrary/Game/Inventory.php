9e13452e7964f3a41eb6f1bfd1996bc9bf0e54f5
Ahmet Türk <ahmetturk93@gmail.com>
2017-07-28 16:37:25 +0300

    FIX ProxySettings

--- src/Fabs/SteamLibrary/Game/Inventory.php
+++ src/Fabs/SteamLibrary/Game/Inventory.php
@@ -16,6 +16,10 @@ class Inventory
 {
     /** @var string */
     public static $proxy_url = null;
+    /** @var int */
+    public static $proxy_port = 0;
+    /** @var string */
+    public static $proxy_username_password = null;
 
     const BASE_IMAGE_URL = 'https://steamcommunity-a.akamaihd.net/economy/image/';
 
@@ -68,6 +72,11 @@ class Inventory
 
             $config = [];
             if (self::$proxy_url !== null) {
+                $config['curl'] = [
+                    CURLOPT_PROXY => self::$proxy_url,
+                    CURLOPT_PROXYPORT => self::$proxy_port,
+                    CURLOPT_PROXYUSERPWD => self::$proxy_username_password,
+                ];
                 $config['proxy'] = self::$proxy_url;
             }
 
