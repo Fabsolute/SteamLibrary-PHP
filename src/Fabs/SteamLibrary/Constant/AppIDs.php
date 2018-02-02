737ac1d795fdd7e231ee0cc838165904a5a9048b
Necip Allef <necipallef@gmail.com>
2018-02-02 10:32:47 +0300

    ADD new constant MAP

--- src/Fabs/SteamLibrary/Constant/AppIDs.php
+++ src/Fabs/SteamLibrary/Constant/AppIDs.php
@@ -10,4 +10,11 @@ class AppIDs
     const CSGO = '730';
     const PUBG = '578080';
     const DOTA_2 = '570';
+
+    const MAP =
+        [
+            self::CSGO => 'CSGO',
+            self::PUBG => 'PUBG',
+            self::DOTA_2 => 'DOTA_2'
+        ];
 }
