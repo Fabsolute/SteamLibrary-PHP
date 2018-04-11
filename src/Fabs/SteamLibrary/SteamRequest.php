7f752f4cc73abe27af25fcc2630c2e0aab21d46b
Necip Allef <necipallef@gmail.com>
2018-04-11 16:25:37 +0300

    CHANGE cookie changed from string to array CHANGE get method now takes 3rd optional parameter json_encode

--- src/Fabs/SteamLibrary/SteamRequest.php
+++ src/Fabs/SteamLibrary/SteamRequest.php
@@ -8,6 +8,7 @@ use Fabs\SteamLibrary\Exception\GeneralSteamException;
 use Fabs\SteamLibrary\Exception\BadGatewayException;
 use Fabs\SteamLibrary\Exception\TooManyRequestException;
 use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
 use GuzzleHttp\Exception\RequestException;
 
 class SteamRequest
@@ -19,21 +20,22 @@ class SteamRequest
     public static $proxy_port = 0;
     /** @var string */
     public static $proxy_username_password = null;
-    /** @var string */
-    public static $cookie = null;
+    /** @var array */
+    public static $cookie = [];
 
     const BASE_IMAGE_URL = 'https://steamcommunity-a.akamaihd.net/economy/image/';
 
     /**
      * @param string $url
      * @param bool $do_not_proxy
+     * @param bool $json_decode
      * @return mixed
      * @throws BadGatewayException
      * @throws GeneralSteamException
      * @throws TooManyRequestException
      * @author necipallef <necipallef@gmail.com>
      */
-    public static function get($url, $do_not_proxy = false)
+    public static function get($url, $do_not_proxy = false, $json_decode = true)
     {
 
         $config = [];
@@ -49,15 +51,25 @@ class SteamRequest
             }
         }
 
-        if (self::$cookie !== null) {
-            $config['headers']['Cookie'] = self::$cookie;
-        }
-
+        $config['cookies'] = CookieJar::fromArray(self::$cookie, 'steamcommunity.com');
         $client = new Client($config);
 
+        $options =
+            [
+                'allow_redirects' =>
+                    [
+                        'max' => 10
+//                        , 'track_redirects' => true
+//                        , 'on_redirect' => SteamRequest::class . '::onRedirect'
+                    ]
+            ];
         try {
-            $json_content = $client->get($url)->getBody()->getContents();
-            return json_decode($json_content, true);
+            $content = $client->get($url, $options)->getBody()->getContents();
+            if ($json_decode === true) {
+                return json_decode($content, true);
+            }
+
+            return $content;
         } catch (RequestException $exception) {
             if ($exception->getResponse() === null) {
                 throw $exception;
@@ -76,6 +88,17 @@ class SteamRequest
         }
     }
 
+//    public static function onRedirect($request, $response, $uri)
+//    {
+//        static $count = 1;
+//        $str_request = var_export($request, true);
+//        $str_response = var_export($response, true);
+//        $str_uri = var_export($uri, true);
+//        file_put_contents('steam_request' . $count . '.txt', $str_request . PHP_EOL, FILE_APPEND);
+//        file_put_contents('steam_request' . $count . '.txt', $str_response . PHP_EOL, FILE_APPEND);
+//        file_put_contents('steam_request' . $count . '.txt', $str_uri . PHP_EOL, FILE_APPEND);
+//        $count++;
+//    }
 
     /**
      * @param string $url
