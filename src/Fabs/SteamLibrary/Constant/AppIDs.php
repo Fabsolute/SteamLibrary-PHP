950bba13e7439e2f15e62a2f13b1c7a61b84c7c5
Necip Allef <necipallef@gmail.com>
2018-05-16 16:25:19 +0300

    ADD h1z1 and tf2

--- src/Fabs/SteamLibrary/Constant/AppIDs.php
+++ src/Fabs/SteamLibrary/Constant/AppIDs.php
@@ -10,11 +10,15 @@ class AppIDs
     const CSGO = '730';
     const PUBG = '578080';
     const DOTA_2 = '570';
+    const H1Z1 = '433850';
+    const TF2 = '440';
 
     const MAP =
         [
             self::CSGO => 'CSGO',
             self::PUBG => 'PUBG',
-            self::DOTA_2 => 'DOTA_2'
+            self::DOTA_2 => 'DOTA_2',
+            self::H1Z1 => 'H1Z1',
+            self::TF2 => 'TF2'
         ];
 }
